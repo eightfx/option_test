20c9aa03732a25e1793194fd3d278d41924c28e8
eight <eight@eightnoiMac.local>
2023-01-27 23:26:07 +0900

    [fix]:

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@ struct Tick{
 	strike: f64,
 	option_type: OptionType,
 	expiry: f64,
-	price: f64,
 	open_interest: f64,
+	implied_volatility: f64,
 }
 
 struct Option{
 	data: Vec<Tick>,
 	option_style: OptionStyle,
+	risk_free_rate: f64,
+	initial_price: f64,
 }
 
 impl Option{
@@ -26,6 +28,8 @@ impl Option{
 		Option{
 			data: Vec::new(),
 			option_style: option_style,
+			risk_free_rate: 0.001,
+			initial_price: 0.0,
 		}
 	}
 	fn add_tick(&mut self, tick: Tick){
@@ -68,8 +72,8 @@ fn main() {
 		strike: 100.0,
 		option_type: OptionType::Call,
 		expiry: 1.0,
-		price: 10.0,
 		open_interest: 100.0,
+		implied_volatility: 0.1,
 	});
 
 	dbg!(option.delta());
