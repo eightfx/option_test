d08cbd221fc943d35a651bc73c73fa4ecb919e2b
eight <eight@eightnoiMac.local>
2023-01-27 23:18:50 +0900

    [update]: definition of trait, struct

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+
+enum OptionType{
+	Put,
+	Call
+}
+
+enum OptionStyle{
+	Europian,
+	American
+}
+struct Tick{
+	strike: f64,
+	option_type: OptionType,
+	expiry: f64,
+	price: f64,
+	open_interest: f64,
+}
+
+struct Option{
+	data: Vec<Tick>,
+	option_style: OptionStyle,
+}
+
+impl Option{
+	fn new(option_style: OptionStyle) -> Option{
+		Option{
+			data: Vec::new(),
+			option_style: option_style,
+		}
+	}
+	fn add_tick(&mut self, tick: Tick){
+		self.data.push(tick);
+	}
+}
+
+trait EuropianGreeks{
+	fn delta(option:&Option) -> f64;
+}
+impl EuropianGreeks for Option{
+	fn delta(option:&Option) -> f64{
+		0.0
+	}
+}
+trait AmericanGreeks{
+	fn delta(option:&Option) -> f64;
+}
+impl AmericanGreeks for Option{
+	fn delta(option:&Option) -> f64{
+		1.0
+	}
+}
+trait Greeks{
+	fn delta(&self) -> f64;
+}
+
+impl Greeks for Option{
+	fn delta(&self) -> f64 {
+		match self.option_style{
+			OptionStyle::Europian => <Option as EuropianGreeks>::delta(self),
+			OptionStyle::American => <Option as AmericanGreeks>::delta(self),
+		}
+	}
+}
+
+fn main() {
+	let option = Option::new(OptionStyle::American);
+	dbg!(option.delta());
+}
