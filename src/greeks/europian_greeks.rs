607cf7bebfc2dca54d58113a2ec09fae45e2ac6f
eight <eight@eightnoiMac.local>
2023-01-30 01:29:11 +0900

    [update]:

--- src/greeks/europian_greeks.rs
+++ src/greeks/europian_greeks.rs
@@ -15,7 +15,7 @@ impl EuropianGreeks for Tick{
 		let t = tick.get_expiry(t);
 		let d1:FloatType;
 		let d2:FloatType;
-		d1 = ((initial_price / &tick.strike).log10() + (risk_free_rate + 0.5 * tick.implied_volatility * tick.implied_volatility) * t) / (&tick.implied_volatility * t.sqrt()) ;
+		d1 = ((initial_price / &tick.strike).log(std::f64::consts::E) + (risk_free_rate + 0.5 * tick.implied_volatility * tick.implied_volatility) * t) / (&tick.implied_volatility * t.sqrt()) ;
 
 		d2 = d1 - &tick.implied_volatility * &t.sqrt();
 
@@ -34,7 +34,7 @@ impl EuropianGreeks for Tick{
 	fn gamma(tick:&Tick, risk_free_rate:&FloatType, initial_price:&FloatType, t:&FloatType) -> FloatType {
 		let (d1, _) = <Tick as EuropianGreeks>::get_d(tick, &risk_free_rate, &initial_price, &t);
 		let t = tick.get_expiry(t);
-		(-0.5 * (d1 * d1)).exp() / ((tick.implied_volatility * initial_price * t.sqrt()) * ((2. * FloatType::from(std::f64::consts::PI)).sqrt()))
+		(-0.5 * d1*d1).exp() / (tick.implied_volatility * initial_price * t.sqrt() *  (2. * FloatType::from(std::f64::consts::PI)).sqrt())
 	}
 
 	fn theta(tick:&Tick, risk_free_rate:&FloatType, initial_price:&FloatType, t:&FloatType) -> FloatType {
@@ -45,7 +45,7 @@ impl EuropianGreeks for Tick{
 			OptionType::Call =>
 				- risk_free_rate * tick.strike * (-risk_free_rate * t).exp() * g.distribution(d2) - tick.implied_volatility * initial_price * (-0.5 * (d1 * d1)).exp() / (2. * t.sqrt() * ((2. * FloatType::from(std::f64::consts::PI)).sqrt())),
 			OptionType::Put =>
-				- risk_free_rate * tick.strike * (-risk_free_rate * t).exp() * (g.distribution(d2)-1.) + tick.implied_volatility * initial_price * (-0.5 * (d1 * d1)).exp() / (2. * t.sqrt() * ((2. * FloatType::from(std::f64::consts::PI)).sqrt())),
+				risk_free_rate * tick.strike * (-risk_free_rate * t).exp() * (g.distribution(-d2)) - tick.implied_volatility * initial_price * (-0.5 * (d1 * d1)).exp() / (2. * t.sqrt() * ((2. * FloatType::from(std::f64::consts::PI)).sqrt())),
 		}
 
 	}
@@ -66,7 +66,56 @@ impl EuropianGreeks for Tick{
 		let (d1, _) = <Tick as EuropianGreeks>::get_d(tick, &risk_free_rate, &initial_price, &t);
 		let t = tick.get_expiry(t);
 
-		tick.strike * t.sqrt() * (-0.5 * (d1 * d1)).exp() / ((2. * FloatType::from(std::f64::consts::PI)).sqrt())
+		initial_price* t.sqrt() * (-0.5 * (d1 * d1)).exp() / ((2. * FloatType::from(std::f64::consts::PI)).sqrt())
 	}
 	
 }
+
+
+#[cfg(test)]
+mod tests{
+	use super::*;
+
+	#[test]
+	fn greeks_call(){
+		let tick = Tick{
+			strike: 250.0,
+			option_type: OptionType::Call,
+			expiry: 60.*60.*24.*30.,
+			open_interest: 0.0,
+			implied_volatility: 10.,
+		};
+		let risk_free_rate = 0.001;
+		let underlying_price = 100.0;
+		let now_timestamp = 0.;
+		
+		assert_float_relative_eq!(tick.delta(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), 0.867,0.01);
+		assert_float_relative_eq!(tick.theta(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), -374.163,0.01);
+		assert_float_relative_eq!(tick.rho(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian),0.818,0.01);
+		assert_float_relative_eq!(tick.gamma(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), 0.0007483,0.0001);
+		assert_float_relative_eq!(tick.vega(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), 6.151,0.01);
+
+	}
+
+	#[test]
+	fn greeks_put(){
+		let tick = Tick{
+			strike: 250.0,
+			option_type: OptionType::Put,
+			expiry: 60.*60.*24.*30.,
+			open_interest: 0.0,
+			implied_volatility: 10.,
+		};
+		let risk_free_rate = 0.001;
+		let underlying_price = 100.0;
+		let now_timestamp = 0.;
+		
+		assert_float_relative_eq!(tick.delta(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), -0.132666,0.01);
+		assert_float_relative_eq!(tick.theta(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), -373.9,0.01);
+		assert_float_relative_eq!(tick.rho(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), -19.7285,0.01);
+		assert_float_relative_eq!(tick.gamma(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), 0.0007483,0.0001);
+		assert_float_relative_eq!(tick.vega(&risk_free_rate, &underlying_price, &now_timestamp, &OptionStyle::Europian), 6.151,0.01);
+
+	}
+	
+} 
