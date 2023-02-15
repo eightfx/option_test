049d909e1f59822f10b6322935ee29aef2485c91
eight <eight@eightnoiMac.local>
2023-02-15 18:45:24 +0900

    [update]: iv_callを計算する時間を表示する処理を追加

--- src/greeks.rs
+++ src/greeks.rs
@@ -32,6 +32,39 @@ macro_rules! greeks_impl {
 	};
 }
 
+// #[derive(FieldNamesAsArray)]
+// pub struct greeks_list {
+// 	pub delta:FloatType,
+// 	pub gamma:FloatType,
+// 	pub theta:FloatType,
+// 	pub vega:FloatType,
+// 	pub rho:FloatType,
+// 	pub epsilon:FloatType,
+// 	pub vanna:FloatType,
+// 	pub charm:FloatType,
+// 	pub vomma:FloatType,
+// 	pub veta:FloatType,
+// 	pub speed:FloatType,
+// 	pub zomma:FloatType,
+// 	pub color:FloatType,
+// 	pub ultima:FloatType,
+// 	pub dual_delta:FloatType,
+// 	pub dual_gamma:FloatType,
+// }
+// macro_rules! greeks_list_trait{
+// 	($pub:vis struct $name:ident {
+// 		$($fpub:vis $field:ident: $type:ty,)*
+// 	}) => {
+// 		pub trait Greeks{
+// 			$(
+// 				fn $field(strike:&FloatType, asset_price:&FloatType, ts_expiration:&FloatType, ts_now:&FloatType, implied_volatility:&FloatType, option_type:&OptionType, option_style:&OptionStyle, risk_free_rate:&FloatType, dividend_yield:&FloatType) -> FloatType;
+// 			)*
+// 		}
+
+// 	}
+// }
+
+// greeks_list_trait!(greeks_list);
 greeks_trait!(delta, gamma, theta, rho, vega, epsilon, vanna, charm, vomma, veta, speed, zomma, color, ultima, dual_delta, dual_gamma);
 greeks_impl!(delta, gamma, theta, rho, vega, epsilon, vanna, charm, vomma, veta, speed, zomma, color, ultima, dual_delta, dual_gamma);
 
