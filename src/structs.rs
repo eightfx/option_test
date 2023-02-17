624faa921ee3e3ec4114c206b055c016e8ffc7ba
eight <eight@eightnoiMac.local>
2023-02-17 16:41:15 +0900

    [update]: BS-model

--- src/structs.rs
+++ src/structs.rs
@@ -1,6 +1,8 @@
 use typed_builder::TypedBuilder;
 
 pub type FloatType = f64;
+
+#[derive(Clone)]
 pub enum OptionType{
 	Put,
 	Call
@@ -12,6 +14,7 @@ pub enum OptionStyle{
 }
 
 
+#[derive(TypedBuilder)]
 #[builder(field_defaults(default=FloatType::NAN, setter(!strip_option)))]
 pub struct OptionTick{
 	pub strike: FloatType,
@@ -19,9 +22,11 @@ pub struct OptionTick{
 	pub open_interest: FloatType,
 	pub implied_volatility: FloatType,
 	pub premium: FloatType,
-	pub initial_price: FloatType,
+	pub asset_price: FloatType,
 	#[builder(default=0.001)]
 	pub risk_free_rate: FloatType,
+	#[builder(default=0.)]
+	pub dividend_yield: FloatType,
 
 	#[builder(default=OptionType::Call)]
 	pub option_type: OptionType,
@@ -30,7 +35,7 @@ pub struct OptionTick{
 }
 
 pub struct Option{
-	pub data: Vec<Tick>,
+	pub data: Vec<OptionTick>,
 	pub option_style: OptionStyle,
 	pub risk_free_rate: FloatType,
 	pub initial_price: FloatType,
@@ -45,12 +50,12 @@ impl Option{
 			initial_price: 0.0,
 		}
 	}
-	pub fn add_tick(&mut self, tick: Tick){
+	pub fn add_tick(&mut self, tick: OptionTick){
 		self.data.push(tick);
 	}
 }
 
-impl Tick{
+impl OptionTick{
 	/// Normalize the difference between maturity and current time
 	///T : 10-digit timestamp
 	///t : 10-digit timestamp
